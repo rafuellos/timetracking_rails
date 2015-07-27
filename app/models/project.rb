688b34fabd5914deeeba32f129bc30cf283d3cf0
Rafael Coomonte <rafuellos@gmail.com>
2015-07-27 17:57:09 +0200

    Seeding database with 25 projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,9 @@ class Project < ActiveRecord::Base
 	def self.iron_find(id)
 		where(id: id).first	
 	end
+
+	def self.clean_all
+		projects = where("created_at <?", Date.current - 1.week)
+		projects.delete_all
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Project.create name: 'Irionhack', description: 'This is the first project for Ironhack venture'
+
+Project.create name: 'Time tracking app'
+
+Project.create name: 'Recipes', description: 'Track my favourite recipes'
+
+22.times do |project| 
+	Project.create name: 'Project ' + project.to_s, description: 'This is project number ' + project.to_s
+end
