f5fda41630523f6bfdc48c57c5de1d482b0349b0
Rafael Coomonte <rafuellos@gmail.com>
2015-07-27 18:00:39 +0200

    Seeding database with 25 projects V2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@ Project.create name: 'Time tracking app'
 Project.create name: 'Recipes', description: 'Track my favourite recipes'
 
 22.times do |project| 
-	Project.create name: 'Project ' + project.to_s, description: 'This is project number ' + project.to_s
+	Project.create name: 'Project #{project}', description: 'This is project number #{project}'
 end
