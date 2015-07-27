0fdfedce5aaf6d90bf12658fe4fca87326bd4f90
Rafael Coomonte <rafuellos@gmail.com>
2015-07-27 12:18:25 +0200

    Create side controller and home and contact responses

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get '/' => 'site#home'
+  get '/contact' => 'site#contactinfo'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
