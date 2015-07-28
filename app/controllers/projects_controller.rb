223289b8c4937de500fc70f78f8dfa60c781ad88
Rafael Coomonte <rafuellos@gmail.com>
2015-07-28 12:47:45 +0200

    Entries creation and seeding

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,13 @@
 class ProjectsController < ApplicationController
 	def index
 		@projects = Project.last_created_projects(10)
+		if @projects.empty?
+			render template: 'projects/no_projects'
+		end
 	end
 	def show
 		unless @project = Project.find_by(id: params[:id])
-		render 'no_projects_found'
+			render 'no_projects_found'
 		end
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,27 @@
 
 22.times do |project| 
-	Project.create name: "Project #{project}", description: "This is project number #{project}"
+	project = Project.create name: "Project #{project}", description: "This is project number #{project}"
+	project.entries.create(hours: 2, minutes: 45)
 end
 
-Project.create name: 'Irionhack', description: 'This is the first project for Ironhack venture'
+project23 = Project.create name: 'Irionhack', description: 'This is the first project for Ironhack venture'
 
-Project.create name: 'Time tracking app'
+project24 = Project.create name: 'Time tracking app'
 
-Project.create name: 'Recipes', description: 'Track my favourite recipes'
+project25 = Project.create name: 'Recipes', description: 'Track my favourite recipes'
+
+
+
+project23.entries.create hours: 1, minutes: 23,
+						 comments: 'Review and update project #{project.id}', 
+						 date: Date.current - 1.day
+
+project23.entries.create hours: 2, minutes: 07,
+						 comments: 'Review and update project #{project.id}', 
+						 date: Date.current
+
+project24.entries.create hours: 0, minutes: 12,
+						 comments: 'Review and update project #{project.id}', 
+						 date: Date.current - 2.days
+
+#Entry.create(project_id: project23.id)
